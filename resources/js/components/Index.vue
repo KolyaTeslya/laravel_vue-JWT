7ee0e5799a32b40cdbf8a27b5ea4776189e169ab
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-06-08 14:07:44 +0300

    creating new routers, new components and views

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link :to="{name: 'fruit.index'}">List</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
