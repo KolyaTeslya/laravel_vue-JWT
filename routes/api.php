40aa240a4866ee25c0afe03244d35859939402c5
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-06-09 22:54:51 +0300

    install jwt token in project

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+
+});
+
 Route::group(['namespace' => 'User', 'prefix' => 'users'], function() {
     Route::post('/', 'StoreController');
 });
