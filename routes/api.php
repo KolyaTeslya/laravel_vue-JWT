99446d8093adbbbf6bf0179b063b7cce9fe51893
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-06-10 14:00:07 +0300

    connect jwt token for login and registration

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,18 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 
-    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function() {
-        Route::get('/', 'IndexController');
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+            Route::get('/', 'IndexController');
+        });
     });
 
 });
 
-Route::group(['namespace' => 'User', 'prefix' => 'users'], function() {
+Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
     Route::post('/', 'StoreController');
 });
 
