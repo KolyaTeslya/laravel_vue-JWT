b4040597bd16ea679531ec072c7ba1c92494c472
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-06-12 16:31:43 +0300

    get refresh token via axios

--- resources/js/api.js
+++ resources/js/api.js
@@ -8,9 +8,8 @@ const  api = axios.create();
 api.interceptors.request.use(config => {
 
     if (localStorage.getItem('access_token')) {
-        config.headers = {
-            'authorization': `Bearer ${localStorage.getItem('access_token')}`
-        };
+        config.headers.authorization = `Bearer ${localStorage.getItem('access_token')}`
+
     }
 
     return config
@@ -21,13 +20,26 @@ api.interceptors.request.use(config => {
 // start response
 api.interceptors.response.use(config => {
     if (localStorage.getItem('access_token')) {
-        config.headers = {
-            'authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
+        config.headers.authorization =`Bearer ${localStorage.getItem('access_token')}`
+
     }
 
     return config
 },error => {
+    if (error.response.data.message === 'Token has expired'){
+        return axios.post('api/auth/refresh', {}, {
+            headers: {
+                'authorization' : `Bearer ${localStorage.getItem('access_token')}`
+            }
+        }).then( res => {
+            localStorage.setItem('access_token', res.data.access_token)
+
+            error.config.headers.authorization =`Bearer ${res.data.access_token}`
+
+            return api.request(error.config)
+
+        })
+    }
     if (error.response.status === 401) {
         router.push({name: ' user.login'})
     }
